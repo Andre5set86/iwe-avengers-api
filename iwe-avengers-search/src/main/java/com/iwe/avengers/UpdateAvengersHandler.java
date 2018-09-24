6efaac1f8f07131ef7301a0ecf152ab32b09493f
Admin <Admin@DESKTOP-LK0OECV>
2018-09-24 20:40:17 -0300

    integrações com Lambda

--- iwe-avengers-search/src/main/java/com/iwe/avengers/UpdateAvengersHandler.java
+++ iwe-avengers-search/src/main/java/com/iwe/avengers/UpdateAvengersHandler.java
@@ -6,21 +6,25 @@ import com.iwe.avenger.dynamodb.entity.Avenger;
 import com.iwe.avenger.lambda.response.HandlerResponse;
 import com.iwe.avengers.dao.AvengerDAO;
 
-public class SearchAvengersHandler implements RequestHandler<Avenger, HandlerResponse> {
+public class UpdateAvengersHandler implements RequestHandler<Avenger, HandlerResponse> {
 
 	
 	private AvengerDAO dao = new AvengerDAO();
 	
 	@Override
 	public HandlerResponse handleRequest(final Avenger avenger, final Context context) {
-	
+		
+		Avenger updatedAvenger = null;
+		
 		context.getLogger().log("[#] - Searching avenger by id:" + avenger.getId());
 		
-		final Avenger avengerFound = dao.search(avenger.getId());
-	
-		final HandlerResponse response = HandlerResponse.builder().setObjectBody(avengerFound).build();
+		if(dao.search(avenger.getId()) != null) {
+			context.getLogger().log("[#] - Avenger Found:" + avenger.getName());
+			updatedAvenger = dao.update(avenger);
+			context.getLogger().log("[#] - Avenger Updated:" + avenger.getName());
+		}
 		
-		context.getLogger().log("[#] - Avenger Found:" + avengerFound.getName());
+		final HandlerResponse response = HandlerResponse.builder().setObjectBody(updatedAvenger).build();
 		
 		return response;
 	
--- iwe-avengers-service/src/main/java/com/iwe/avengers/dao/AvengerDAO.java
+++ iwe-avengers-service/src/main/java/com/iwe/avengers/dao/AvengerDAO.java
@@ -7,11 +7,11 @@ import com.iwe.avenger.dynamodb.entity.Avenger;
 
 public class AvengerDAO {
 
-	private Map<String, Avenger> mapper = new HashMap<String, Avenger>();
+	private Map<String, Avenger> mapper; 
 	
 	
 	public AvengerDAO() {
-		
+		mapper = new HashMap<String, Avenger>();
 		mapper.put("aaaa-bbbb-cccc-dddd", new Avenger("aaaa-bbbb-cccc-dddd", "Capitain America", "Steve Rogers"));
 		mapper.put("aaaa-bbbb-cccc-eeee", new Avenger("aaaa-bbbb-cccc-eeee", "Black Widow", "Natasha Romanov"));
 		mapper.put("aaaa-bbbb-cccc-ffff", new Avenger("aaaa-bbbb-cccc-ffff", "Iron Man", "Tony Stark"));
@@ -21,6 +21,11 @@ public class AvengerDAO {
 	
 	public Avenger search(String id) {
 		return mapper.get(id);
+	}
+
+	public Avenger update(Avenger avenger) {
+		// TODO Auto-generated method stub
+		return mapper.replace(avenger.getId(), avenger);
 	}
 
 }
