55f4b510db9414f4b2b93420bdc4330f174a767d
Admin <Admin@DESKTOP-LK0OECV>
2018-09-24 20:46:13 -0300

    update

--- iwe-avengers-update/src/main/java/com/iwe/avengers/UpdateAvengersHandler.java
+++ iwe-avengers-update/src/main/java/com/iwe/avengers/UpdateAvengersHandler.java
@@ -4,11 +4,28 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.iwe.avenger.dynamodb.entity.Avenger;
 import com.iwe.avenger.lambda.response.HandlerResponse;
+import com.iwe.avengers.dao.AvengerDAO;
 
 public class UpdateAvengersHandler implements RequestHandler<Avenger, HandlerResponse> {
 
+	private AvengerDAO dao = new AvengerDAO();
+	
 	@Override
 	public HandlerResponse handleRequest(final Avenger avenger, final Context context) {
-		return null;
+		
+		Avenger updatedAvenger = null;
+		
+		context.getLogger().log("[#] - Searching avenger by id:" + avenger.getId());
+		
+		if(dao.search(avenger.getId()) != null) {
+			context.getLogger().log("[#] - Avenger Found:" + avenger.getName());
+			updatedAvenger = dao.update(avenger);
+			context.getLogger().log("[#] - Avenger Updated:" + avenger.getName());
+		}
+		
+		final HandlerResponse response = HandlerResponse.builder().setObjectBody(updatedAvenger).build();
+		
+		return response;
+	
 	}
 }
